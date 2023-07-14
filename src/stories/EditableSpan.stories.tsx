e926c9a84b120ddfe162d36cf0741f9ecaa5f428
OksanaN <nurova.oksana2020@gmail.com>
2023-07-14 10:46:22 +0500

    add snapshot tests

---
+++ src/stories/EditableSpan.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import {EditableSpan} from "../EditableSpan";
+import {Provider} from "react-redux";
+import { store } from '../state/store';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+
+const meta: Meta<typeof EditableSpan> = {
+  title: 'TODOLISTS/EditableSpan',
+  component: EditableSpan,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+};
+
+export default meta;
+type Story = StoryObj<typeof EditableSpan>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+export const EditableSpanStory: Story = {
+  // More on args: https://storybook.js.org/docs/react/writing-stories/args
+  render: () => <Provider store={store}><EditableSpan value={'test'} todolistId={'dfgfdgdf'}/></Provider>
+}
